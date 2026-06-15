f4bb352d586933c2032ee9d98e3b4e9a748c396d
sergxlove <sergxlove@mail.ru>
2026-06-16 02:28:01 +0600

    add db project

---
+++ AiAgentManager.DataBase.Sqlite/AiAgentManagerDbContext.cs
@@ -0,0 +1,28 @@
+﻿using AiAgentManager.DataBase.Sqlite.Configurations;
+using AiAgentManager.DataBase.Sqlite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AiAgentManager.DataBase.Sqlite
+{
+    public class AiAgentManagerDbContext : DbContext
+    {
+        public AiAgentManagerDbContext(DbContextOptions<AiAgentManagerDbContext> options)
+            :base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<SavedAgentsEntity> SavedAgents { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new SavedAgentsConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ AiAgentManager.DataBase.Sqlite/Interfaces/ISavedAgentsRepository.cs
@@ -0,0 +1,13 @@
+﻿using AiAgentManager.Core.Models;
+
+namespace AiAgentManager.DataBase.Sqlite.Interfaces
+{
+    public interface ISavedAgentsRepository
+    {
+        Task<Guid> AddAsync(SavedAgents agent, CancellationToken token);
+        Task<bool> CheckAsync(string name, CancellationToken token);
+        Task<int> DeleteAsync(string name, CancellationToken token);
+        Task<List<SavedAgents>> GetAllAsync(CancellationToken token);
+        Task<int> RenameUpdateAsync(string oldName, string newName, CancellationToken token);
+    }
+}
