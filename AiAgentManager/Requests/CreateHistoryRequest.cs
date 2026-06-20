54e079905c4989c84213cf084868ceb6c94c5530
sergxlove <sergxlove@mail.ru>
2026-06-20 20:29:05 +0600

    add new endpoints

---
+++ AiAgentManager/Requests/CreateHistoryRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace AiAgentManager.Requests
+{
+    public class CreateHistoryRequest
+    {
+        public Guid ChatId { get; set; }
+        public string SenderMessage { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+    }
+}
